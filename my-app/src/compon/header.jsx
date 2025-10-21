6cd363956946f99762ea7006b7cd0ca8fe90f7bd
Nks225 <goncarovnikita035@gmail.com>
2025-10-21 22:41:00 +1000

    test1

---
+++ my-app/src/compon/header.jsx
@@ -0,0 +1,26 @@
+import react from "react";
+
+function header() {
+    <header>
+        <h1>E-STORE</h1>
+        <nav>
+            <ul>
+                <li>
+                    <a href="#home">Главная</a>
+                </li>
+                <li>
+                    <a href="#catalog">Главная</a>
+                </li>
+                <li>
+                    <a href="#about">О нас</a>
+                </li>
+                <li>
+                    <a href="#home">Главная</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+
+}
+
+export default header();
